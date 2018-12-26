450096b3a6c00eb70f71db04b02346b20f79bd75
Fukuda Naoto <never9612@gmail.com>
2018-12-26 21:08:52 +0900

    edit typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,22 @@
 import Typography from 'typography'
 import Wordpress2016 from 'typography-theme-wordpress-2016'
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    'a.gatsby-resp-image-link': {
-      boxShadow: `none`,
-    },
-  }
-}
+const googleFonts = [
+  {
+    name: 'M PLUS 1p',
+    styles: ['400'],
+  },
+  {
+    name: 'M PLUS Rounded 1c',
+    styles: ['400'],
+  },
+]
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography({
+  googleFonts: googleFonts,
+  headerFontFamily: ['M PLUS 1p'],
+  bodyFontFamily: ['M PLUS Rounded 1c','YuGothic','Yu Gothic','sans-serif'],
+})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
